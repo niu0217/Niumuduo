57f7dc2cdb12bf4c36966b38b00fecdcf5cd7a4e
niu0217 <1033856427@qq.com>
2024-05-15 16:07:05 +0800

    注释：线程代码

--- muduo/base/CurrentThread.cc
+++ muduo/base/CurrentThread.cc
@@ -13,10 +13,10 @@ namespace muduo
 {
 namespace CurrentThread
 {
-__thread int t_cachedTid = 0;
-__thread char t_tidString[32];
+__thread int t_cachedTid = 0;  // 当前线程的tid缓存
+__thread char t_tidString[32]; // tid的字符串表示形式
 __thread int t_tidStringLength = 6;
-__thread const char* t_threadName = "unknown";
+__thread const char* t_threadName = "unknown"; // 当前线程的名字
 static_assert(std::is_same<int, pid_t>::value, "pid_t should be int");
 
 string stackTrace(bool demangle)
